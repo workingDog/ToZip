0d207fde7b2d2c757533fdb4392148cccd3acd90
workingdog <ringowathelet@gmail.com>
2026-02-22 10:48:50 +0900

    Initial Commit

---
+++ ToZip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToZip
+//
+//  Created by Ringo Wathelet on 2026/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToZip/ToZipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToZipApp.swift
+//  ToZip
+//
+//  Created by Ringo Wathelet on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ToZipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
